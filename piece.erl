b012fffbe60d945a4e43c819f5ef50f6357f0a10
John Daily <git@epep.us>
2012-08-31 22:31:49 -0400

    Be explicit about move vs capture, and start deploying the move functions in the code for retrieval

--- piece.erl
+++ piece.erl
@@ -8,6 +8,8 @@
 -module(piece).
 -compile(export_all).
 
+
+
 %% @doc Maneuver is a function that determines the validity of the
 %% attempted move, Location is an {x, y} tuple that indicates where
 %% the piece is at the moment, and Team is black or white.
@@ -17,15 +19,19 @@
 %% whether there are pieces in my way, etc.
 %%
 %% First step: just make the move.
-piece_loop(Maneuver, Location, Team) ->
+piece_loop(Move, Capture, Location, Team) ->
     receive
-	{ From, X, Y } when X > 0, X < 9, Y > 0, Y < 9 ->
-            piece_loop(Maneuver,
-                       move_response(From, Location, Maneuver(Location, {X, Y})),
+	{ From, move, X, Y } when X > 0, X < 9, Y > 0, Y < 9 ->
+            piece_loop(Move, Capture,
+                       move_response(From, Location, Move(Location, {X, Y}, Team)),
+                       Team);
+	{ From, capture, X, Y } when X > 0, X < 9, Y > 0, Y < 9 ->
+            piece_loop(Move, Capture,
+                       move_response(From, Location, Capture(Location, {X, Y}, Team)),
                        Team);
         { From, location } ->
 	    From ! Location,
-            piece_loop(Maneuver, Location, Team);
+            piece_loop(Move, Capture, Location, Team);
         done ->
             done
     end.
@@ -38,6 +44,20 @@ move_response(PID, _, Loc) ->
     PID ! { yes, Loc },
     Loc.
 
-	    
 
+movefuns() ->
+    [ 
+      { pawnmove, fun({ X, OldY }, { X, NewY }, white) when NewY - OldY =:= 1 -> { X, NewY };
+                     ({ X, OldY }, { X, NewY }, black) when NewY - OldY =:= -1 -> { X, NewY };
+                     ({ X, 2 }, { X, 4 }, white) -> { X, 4 };
+                     ({ X, 7 }, { X, 5 }, black) -> { X, 5 };
+                     (Loc, _, _) -> Loc end
+      },
 
+      { pawncap, fun({ OldX, OldY }, { NewX, NewY }, white)
+                       when abs(NewX - OldX) =:= 1, NewY - OldY =:= 1 -> { NewX, NewY };
+                    ({ OldX, OldY }, { NewX, NewY }, black)
+                       when abs(NewX - OldX) =:= 1, NewY - OldY =:= -1 -> { NewX, NewY };
+                     (Loc, _, _) -> Loc end
+      }
+    ].
