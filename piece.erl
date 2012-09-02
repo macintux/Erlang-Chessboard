830d23d4c2a42355de37a8c24523fb6be03260ca
John Daily <git@epep.us>
2012-09-02 16:00:14 -0400

    Remove comments that I am not keeping updated - blog captures all the logic anyway

--- piece.erl
+++ piece.erl
@@ -8,17 +8,6 @@
 -module(piece).
 -compile(export_all).
 
-
-
-%% @doc Maneuver is a function that determines the validity of the
-%% attempted move, Location is an {x, y} tuple that indicates where
-%% the piece is at the moment, and Team is black or white.
-%%
-%% Eventually I'll need additional PIDs to be passed in, like an
-%% object to decide whether I'm pinned, something to help me decide
-%% whether there are pieces in my way, etc.
-%%
-%% First step: just make the move.
 piece_loop(Move, Capture, Location, Team) ->
     receive
         { From, move, {X, Y} } when X > 0, X < 9, Y > 0, Y < 9 ->
@@ -42,7 +31,6 @@ piece_loop(Move, Capture, Location, Team) ->
             throw({unknown_message, Message})
     end.
 
-%% @doc Tell the process which asked us to move whether the piece moved.
 move_response(PID, Loc, Loc) ->
     PID ! { no, Loc },
     Loc;
